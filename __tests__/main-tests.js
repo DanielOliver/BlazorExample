3cd533cc4901b1fd77e85cd3e61e0285685bb335
Daniel Oliver <danieloliversoftware@gmail.com>
2018-11-25 02:36:57 -0500

    Add jest tests.

---
+++ __tests__/main-tests.js
@@ -0,0 +1,18 @@
+describe('Google', () => {
+    beforeAll(async () => {
+        await page.goto('http:localhost:5000', { waitUntil : 'networkidle0' } );
+    });
+
+    it('should display "Hello, world!" text on page', async () => {
+        await expect(page).toMatch('Hello, world!');
+    });
+
+    
+    it('should navigate to counter page', async () => {
+        await page.click('a[href=counter]');
+        await expect(page).toMatch('Current count: 0');
+
+        await page.click('button.btn');
+        await expect(page).toMatch('Current count: 1');
+    });
+});
