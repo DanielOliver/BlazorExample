d7f7dcf98aa0222c310b33ac14c850c369b6f0da
Daniel Oliver <danieloliversoftware@gmail.com>
2018-11-25 02:45:51 -0500

    clean up config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,5 +3,9 @@ module.exports = {
       dumpio: true,
       headless: process.env.HEADLESS !== 'false',
     },
+    server: {
+      command: 'dotnet run --project Blazor.Client',
+      port: 5000,
+    },
     browserContext: 'default',
   }
